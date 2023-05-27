e9f73d568c23b81c83b1fa65ece31cb2106796c7
IlanVaks <ilanvaks@gmail.com>
2023-05-27 13:00:44 -0400

    adding-jtoken

--- functions/src/dbConnect.js
+++ functions/src/dbConnect.js
@@ -4,6 +4,6 @@ import { mongoURI } from "../secrets.js";
 const client = new MongoClient(mongoURI)
 
 export const db = client.db('lacetalk')
-
+export const userCollection = db.collection('users')
 
 
