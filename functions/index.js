f93bf8f5e34279a23c19c0466bebd5dae1a1ac5f
IlanVaks <ilanvaks@gmail.com>
2023-06-04 14:05:45 -0400

    Working-JWT

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-
 import { getAllSneakers, addSneaker, updateSneaker, deleteSneaker, voteUpSneaker, voteDownSneaker } from "./src/sneakers.js"
+import { loginUser, registerUser } from "./src/users.js"
 
 const PORT = 3000
 const app = express()
@@ -18,6 +18,9 @@ app.post('/sneakers/:sneakerId/voteUp', voteUpSneaker);
 app.post('/sneakers/:sneakerId/voteDown', voteDownSneaker);
 app.delete('/sneaker/:sneakerId', deleteSneaker)
 app.patch('/sneaker/:sneakerId', updateSneaker)
+
+app.post('/join', loginUser)
+app.post('/register', registerUser) //signup
 
 // app.listen(PORT, () => {
 //   console.log(`Listening on http://localhost:${PORT}...`)
--- functions/src/users.js
+++ functions/src/users.js
@@ -1,25 +1,34 @@
 import { db } from "./dbConnect.js";
-import bcrypt from 'bcryptjs';
+import { hashSync } from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { ObjectId } from "mongodb";
+import { secretKey, salt } from '../secrets.js';
 
 const userColl = db.collection("users");
 
 export async function registerUser(req, res) {
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  await userColl.insertOne({ username, password: hashedPassword });
+  const { email, password } = req.body;
+
+  const check = await userColl.findOne({email: email.toLowerCase()})
+  if(check) {
+    res.status(401).send({message: 'Email already in use, please try logging in.'})
+    return
+  }
+
+  const hashedPassword = hashSync(password, salt);
+  await userColl.insertOne({ email: email.toLowerCase(), password: hashedPassword }); 
   res.status(201).send({ message: "User registered" });
 }
 
 export async function loginUser(req, res) {
-  const { username, password } = req.body;
-  const user = await userColl.findOne({ username });
-  if (!user) return res.status(400).send('Invalid username or password.');
+  const { email, password } = req.body;
 
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (!validPassword) return res.status(400).send('Invalid username or password.');
+  const hashedPassword = hashSync(password, salt); // Hash the password first
 
+  const user = await userColl.findOne({ email: email.toLowerCase(), password: hashedPassword });
+  if (!user) return res.status(400).send('Invalid email or password.');
+
+  delete user.password; 
   const token = generateAuthToken(user);
   res.header('x-auth-token', token).send({
       _id: user._id,
@@ -27,8 +36,9 @@ export async function loginUser(req, res) {
   });
 }
 
+
 function generateAuthToken(user) {
-  return jwt.sign({ _id: user._id }, 'your secret key', { expiresIn: '1h' });
+  return jwt.sign(user, secretKey, { expiresIn: '1h' }); 
 }
 
 export const auth = async (req, res, next) => {
@@ -36,10 +46,26 @@ export const auth = async (req, res, next) => {
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
   try {
-      const decoded = jwt.verify(token, 'your secret key');
+      const decoded = jwt.verify(token, secretKey);
       req.user = decoded;
       next();
   } catch (ex) {
       res.status(400).send('Invalid token.');
   }
+}
+
+// Add getUser function to retrieve user based on id
+export async function getUser(req, res) {
+  const { id } = req.params;
+  const user = await userColl.findOne({ _id: new ObjectId(id) });
+  if (!user) return res.status(404).send('User not found.');
+  res.send(user);
+}
+
+// Add deleteUser function to delete user based on id
+export async function deleteUser(req, res) {
+  const { id } = req.params;
+  const result = await userColl.deleteOne({ _id: new ObjectId(id) });
+  if (result.deletedCount === 0) return res.status(404).send('User not found.');
+  res.send({ message: "User deleted" });
 }
